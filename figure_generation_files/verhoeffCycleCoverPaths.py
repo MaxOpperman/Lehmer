9119388be69a23b2930d1e07f3ae8860726f9c14
MaxOpperman <m.opperman98@gmail.com>
2024-08-12 11:26:11 +0200

    Add the e-f-cycle on odd-2-1 signatures

--- figure_generation_files/verhoeffCycleCoverPaths.py
+++ figure_generation_files/verhoeffCycleCoverPaths.py
@@ -190,6 +190,107 @@ def fig12(ax, even_int: int):
     ax.axis("off")
 
 
+def fig12_e_f(ax, even_int: int):
+    """
+    Generate and plot Figure 12 from Verhoeff's paper (see References). The figure displays a path between `e` and `f`:\n
+        - `e0 = 1 02 0^{k_0-2} 1 0`
+        - `f0 = 0 12 0^{k_0-2} 1 0`\n
+    where `k_0` is an even integer (`even_int`). The path is highlighted in blue and important nodes are marked.
+
+    Args:
+        ax: The matplotlib Axes object to plot the figure on.
+        even_int: An even integer value.
+
+    Returns:
+        None: Works in place to plot the figure on the given `ax`.
+
+    References:
+        - Tom Verhoeff. The spurs of D. H. Lehmer: Hamiltonian paths in neighbor-swap graphs of permutations. Designs, Codes, and Cryptography, 84(1-2):295-310, 7 2017.
+    """
+    diff_4 = even_int - 4
+
+    # Set limits and aspect ratio
+    x, y = 3 + diff_4, 5 + diff_4
+    ax.set_xlim(-0.5, x + 0.5)
+    ax.set_ylim(-0.5, y + 0.5)
+    ax.set_aspect("equal")
+
+    # Draw vertical lines
+    for y_line in range(x + 1):
+        ax.plot([y_line, y_line], [0, y], color="black", linewidth=1)
+
+    # Draw horizontal lines except for the diagonal positions where x == y
+    for y_line in range(y + 1):
+        for x_line in range(x):
+            if x_line != y_line - 1:
+                ax.plot(
+                    [x_line, x_line + 1], [y_line, y_line], color="black", linewidth=1
+                )
+
+    # Highlighted path coordinates
+    generated_tuples = []
+    for i in range(0, diff_4, 2):
+        generated_tuples.append((x - i, i))
+        generated_tuples.append((x - i, y - i))
+        generated_tuples.append((0, y - i))
+        generated_tuples.append((0, y - i - 1))
+        generated_tuples.append((x - i - 1, y - i - 1))
+        generated_tuples.append((x - i - 1, 1))
+        generated_tuples.append((x - i - 2, 1))
+    if even_int == 4:
+        start = (3, 0)
+    else:
+        start = (3, 1)
+    path = (
+        [(0, 2), (0, 0)]
+        + generated_tuples
+        + [
+            start,
+            (3, 5),
+            (0, 5),
+            (0, 3),
+            (1, 3),
+            (1, 4),
+            (2, 4),
+            (2, 1),
+            (1, 1),
+            (1, 2),
+        ]
+    )
+    path_x, path_y = zip(*path)
+
+    # Draw highlighted path
+    ax.plot(path_x, path_y, color="blue", linewidth=5)
+    ax.plot([0, 1], [2, 2], color="orange", linewidth=5)
+
+    # Draw black circles at key points
+    key_points = [(0, 2), (0, 0), (x, 0), (x, y), (0, y), (1, 2)]
+    for point in key_points:
+        ax.plot(*point, "ko", markersize=10)
+
+    # Add text annotations
+    odd_int = even_int - 1
+    annotations = {
+        (0, y + 0.5): f'1{odd_int*"0"}12',
+        (x, y + 0.5): f'{odd_int*"0"}112',
+        (x, -0.5): f'2{odd_int*"0"}11',
+        (0, -0.5): f'21{odd_int*"0"}1',
+        (((odd_int + 1.1) * -0.22), 2.0): f'e=102{(odd_int-1)*"0"}1',
+        (0.8, 2.3): f'f=012{(odd_int-1)*"0"}1',
+    }
+    for (x, y), text in annotations.items():
+        ax.text(x, y, text, fontsize=12, ha="center", va="center")
+
+    # Annotate points e and f
+    # ax.text(-0.25, 1, "a", fontsize=12, ha="right", va="center")
+    # ax.text(0.75, 1, "b", fontsize=12, ha="right", va="center")
+    # ax.text(-0.25, 2, "e", fontsize=12, ha="right", va="center")
+    # ax.text(1.25, 2.25, "f", fontsize=12, ha="right", va="center")
+
+    # Hide axes
+    ax.axis("off")
+
+
 def combined_figure(even_int: int, save=False):
     """
     Generate and plot both Figure 11 and Figure 12 from Verhoeff's paper (see References) in a single plot.
@@ -225,7 +326,7 @@ def combined_figure(even_int: int, save=False):
         plt.show()
 
 
-def plot_individual_figures(even_int: int, save=False):
+def plot_individual_figures(even_int: int, save=False, cycle=False):
     """
     Generate and plot Figure 11 and Figure 12 from Verhoeff's paper (see References) in separate plots.
     The figures display paths between `c` and `d` and between `a` and `b` respectively. The paths are highlighted in blue and important nodes are marked.
@@ -241,6 +342,21 @@ def plot_individual_figures(even_int: int, save=False):
     References:
         - Tom Verhoeff. The spurs of D. H. Lehmer: Hamiltonian paths in neighbor-swap graphs of permutations. Designs, Codes, and Cryptography, 84(1-2):295-310, 7 2017.
     """
+
+    if cycle:
+        fig, ax = plt.subplots()
+        fig12_e_f(ax, even_int)
+        if save:
+            if not os.path.exists("./out"):
+                os.makedirs("./out")
+            plt.savefig(
+                f"./out/Cycle{even_int-1}21And{even_int-1}11.png",
+                dpi=300,
+                bbox_inches="tight",
+            )
+        else:
+            plt.show()
+        return
     # Plot fig11
     fig, ax = plt.subplots()
     fig11(ax, even_int)
@@ -277,12 +393,15 @@ if __name__ == "__main__":
         "-s", "--save", action="store_true", help="Save image in ./out directory"
     )
     parser.add_argument(
-        "-c", "--combine", action="store_true", help="Combine the graphs into one plot"
+        "-m", "--merge", action="store_true", help="Merge the graphs into one plot"
+    )
+    parser.add_argument(
+        "-c", "--cycle", action="store_true", help="Generate a cycle instead of a path"
     )
 
     args = parser.parse_args()
 
-    if args.combine:
+    if args.merge:
         combined_figure(args.even, args.save)
     else:
-        plot_individual_figures(args.even, args.save)
+        plot_individual_figures(args.even, args.save, args.cycle)
