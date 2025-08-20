f665a8d05af9f3e42ad4420e445ed5483d2bf6eb
Max <m.opperman98@gmail.com>
2025-08-20 21:55:35 +0200

    chore: fix api url

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import GraphVisualization from "./components/GraphVisualization.vue";
 import { BackendEdge, generateEdges, VisualizationNode } from "./utils/edgeGenerator";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5050";
+  import.meta.env.API_URL || "http://localhost:5050";
 const signatureInput = ref("");
 
 const nodes = ref<VisualizationNode[]>([]);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    __APP_VERSION__: JSON.stringify('v1.0.0'),
+    __API_URL__: JSON.stringify('http://lehmer_backend:5050'),
+  },
 });
